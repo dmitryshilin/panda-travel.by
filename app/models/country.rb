eeb941a47c8b8f3430a33728904457bd5e525aa5
Alexey Ershov <alex4rom@gmail.com>
2013-09-17 15:56:39 +0300

    Add country spec, country model validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -3,4 +3,10 @@ class Country < ActiveRecord::Base
   has_many :article_countries
   has_many :tours, through: :country_tours
   has_many :country_tours
+
+  validates :title, :description, :rating, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: { in: 2..30 }
+  validates :description, length: { in: 5..250 }
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
 end
---
+++ spec/models/country_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Country do
+
+  context '#associations' do
+    it { should have_many(:articles).through(:article_countries) }
+    it { should have_many(:article_countries) }
+    it { should have_many(:tours).through(:country_tours) }
+    it { should have_many(:country_tours) }
+  end
+
+  context '#validations' do
+    [:title, :description, :rating].each do
+      |value| it { should validate_presence_of(value) }
+    end
+
+    it { should validate_uniqueness_of(:title) }
+    it { should ensure_length_of(:title).is_at_least(2).is_at_most(30) }
+    it { should ensure_length_of(:description).is_at_least(5).is_at_most(250) }
+
+    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
+  end
+
+end
