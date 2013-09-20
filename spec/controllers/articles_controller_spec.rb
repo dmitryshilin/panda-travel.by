128a1491a144b0adce3529e7a32bba1e0a8e1c1f
avamel <xnuamx@gmail.com>
2013-09-20 14:35:29 +0300

    init commit

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe ArticlesController do
+  let(:new_article) { FactoryGirl.build(:article) }
+  let(:published_article) { FactoryGirl.create(:pub_article)}
+  let(:unpublished_article) {FactoryGirl.create(:unpub_article)}
+  let(:user) {User.new}
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      visit articles_path
       response.should be_success
     end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      visit article_path(new_article)
       response.should be_success
     end
   end
+
+  describe "ge"
 
 end
