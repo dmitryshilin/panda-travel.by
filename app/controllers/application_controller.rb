eaa25a9808e9b3b8d74bc76cce9285016b463e1c
dmitryshilin <dimashilin@gmail.com>
2013-09-20 14:33:33 +0300

    cancan added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # check_authorization
   protect_from_forgery with: :exception
   before_action :constant
   layout :choose_layout
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 
   def constant
     @europe = Country.where('region = ?', 'Европа')
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
+require "cancan/matchers"
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe "abilities" do
+  #   subject(:ability){ Ability.new(user) }
+  #   let(:user){ nil }
+
+  #   context "when is an account manager" do
+  #     let(:user){ FactoryGirl(:accounts_manager) }
+
+  #     it{ should be_able_to(:manage, AdminUser.new) }
+  #   end
+  # end
 end
