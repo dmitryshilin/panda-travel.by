f50c1546b87b92c8c2fcb51616455894c1313e9e
Alexey Ershov <alex4rom@gmail.com>
2013-09-23 13:31:16 +0300

    Add countries view specs

--- spec/views/countries/index.html.haml_spec.rb
+++ spec/views/countries/index.html.haml_spec.rb
@@ -5,9 +5,13 @@ describe 'countries/index.html.haml' do
 
   it 'displays all the country titles' do
     assign(:countries, [country])
-
     render
-
     expect(rendered).to match country.title
+  end
+
+  it 'displays the country titles links' do
+    assign(:countries, [country])
+    render
+    rendered.should have_link(country.title)
   end
 end
