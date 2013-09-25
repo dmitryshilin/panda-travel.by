3d52a0e152c5656ef572ef77be9a0c7655f4bb34
dmitryshilin <dimashilin@gmail.com>
2013-09-25 12:17:56 +0300

    database_cleaner back again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,15 +42,15 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   # include database_cleaner
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :truncation
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
