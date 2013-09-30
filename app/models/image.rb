e0cdb41b7b39f28b24bcbd564aad7b95fa3ae336
avamel <xnuamx@gmail.com>
2013-09-30 16:32:07 +0300

    add tour image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
   belongs_to :tour
-  has_attached_file :image, styles: { thumb: '320x236#' }
+  has_attached_file :image, styles: { small: '70x60#',medium: '320x236#', large: '670x340#' }
 end
