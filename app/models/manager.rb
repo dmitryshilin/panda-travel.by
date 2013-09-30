fc1f0af170e45b8cbb3446f0233fd6b15cb1b0ec
avamel <xnuamx@gmail.com>
2013-09-30 18:43:15 +0300

    add some panel

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -3,7 +3,7 @@ class Manager < ActiveRecord::Base
   has_many :tours, through: :manager_tours
   has_many :manager_tours
   accepts_nested_attributes_for :contacts, allow_destroy: true
-  has_attached_file :avatar, styles: { thumb: '96x96#' }
+  has_attached_file :avatar, styles: { small: '96x96#' }
   has_destroyable_file :avatar_delete
 
   validates_presence_of :name
