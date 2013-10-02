b023b4f6e32fb835e8022ee25fe92ea630833955
dmitryshilin <dimashilin@gmail.com>
2013-10-02 16:32:11 +0300

    some styles home page

---
+++ app/assets/javascripts/import/home.js
@@ -0,0 +1,21 @@
+$(function(){
+      var $container = $('#hits_of_sales');
+      $container.imagesLoaded( function() {
+          $container.masonry({
+              isFitWidth: true,
+              columnWidth: 260,
+              itemSelector: '.hit-item'
+          });
+      });
+  });
+$(function(){
+        var $container = $('#articles');
+      $container.imagesLoaded( function() {
+          $container.masonry({
+              isFitWidth: true,
+              columnWidth: 260,
+              itemSelector: '.art-item'
+          });
+      });
+
+});
