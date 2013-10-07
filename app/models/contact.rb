771e28ba654a8356625ca4e4ced83d2b442182a8
dmitryshilin <dimashilin@gmail.com>
2013-10-07 12:16:23 +0300

    some features on tour done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,5 +6,4 @@ class Contact < ActiveRecord::Base
   validates_presence_of :contact_type, :description
   validates_length_of :contact_type, within: 3..20
   validates_length_of :description, within: 5..40
-
 end
